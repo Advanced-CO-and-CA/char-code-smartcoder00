3c677d7b53825a2e8d4db8b4a7a0f41b9c412c51
Jethin Sekhar R <jethin@qualcomm.com>
2019-10-19 23:43:11 +0530

    Corrected Allignments

--- char_code.s
+++ char_code.s
@@ -10,13 +10,13 @@
 
 @ data section
     .data
-    result_start:         .word 0                   @ Part1 Test A Result 
-                          .word 0                   @ Part1 Test B Result 
-                          .word 0                   @ Part1 Test C Result 
-                          .word 0                   @ Part2 Test A Result 
-                          .word 0                   @ Part2 Test B Result 
-                          .word 0                   @ Part2 Test C Result 
-						  
+    result_start:         .word 0                   @ Part1 Test A Result
+                          .word 0                   @ Part1 Test B Result
+                          .word 0                   @ Part1 Test C Result
+                          .word 0                   @ Part2 Test A Result
+                          .word 0                   @ Part2 Test B Result
+                          .word 0                   @ Part2 Test C Result
+
     part1_str_len:        .word     3
     part1_str1:           .ascii    "CAT"
     part1_str2:           .ascii    "BAT"
@@ -61,50 +61,50 @@ _main:
     mov  temp, #0
     ldr  param1, =part1_str1                        @ Part 1 TestA "CAT" & "BAT"
     ldr  param2, =part1_str2                        @   Params String1, String2, Length
-    ldr  param3, =part1_str_len                     @ 
+    ldr  param3, =part1_str_len                     @
     ldr  param3, [param3]                           @
     bl   _fnCompareString                           @ Function Call to Compare String
-	mov   param1, temp                              @ Store the Result and increment
-	bl   _fnStoreResult                             @    Result Store Index
-	add  temp, #4                                   @ 
+    mov   param1, temp                              @ Store the Result and increment
+    bl   _fnStoreResult                             @    Result Store Index
+    add  temp, #4                                   @
 
     ldr  param1, =part1_str1                        @ Part 1 TestB "CAT" & "CAT"
     ldr  param2, =part1_str1                        @   Params String1, String2, Length
-    ldr  param3, =part1_str_len                     @ 
+    ldr  param3, =part1_str_len                     @
     ldr  param3, [param3]                           @
     bl   _fnCompareString                           @ Function Call to Compare String
-	mov   param1, temp                              @ Store the Result and increment
-	bl   _fnStoreResult                             @    Result Store Index
-	add  temp, #4                                   @ 
+    mov   param1, temp                              @ Store the Result and increment
+    bl   _fnStoreResult                             @    Result Store Index
+    add  temp, #4                                   @
 
     ldr  param1, =part1_str1                        @ Part 1 TestB "CAT" & "CUT"
     ldr  param2, =part1_str3                        @   Params String1, String2, Length
-    ldr  param3, =part1_str_len                     @ 
-    ldr  param3, [param3]                           @ 
+    ldr  param3, =part1_str_len                     @
+    ldr  param3, [param3]                           @
     bl   _fnCompareString                           @ Function Call to Compare String
-	mov   param1, temp                              @ Store the Result and increment
-	bl   _fnStoreResult                             @    Result Store Index
-	add  temp, #4                                   @ 
+    mov   param1, temp                              @ Store the Result and increment
+    bl   _fnStoreResult                             @    Result Store Index
+    add  temp, #4                                   @
 
     ldr  param1, =part2_str1                        @ Part 2 TestA "CS6620" & "SS"
     ldr  param2, =part2_str2                        @   Params String1, String2
     bl   _fnFindSubString                           @
-	mov   param1, temp                              @ Store the Result and increment
-	bl   _fnStoreResult                             @    Result Store Index
-	add  temp, #4                                   @ 
+    mov   param1, temp                              @ Store the Result and increment
+    bl   _fnStoreResult                             @    Result Store Index
+    add  temp, #4                                   @
 
     ldr  param1, =part2_str1                        @ Part 2 TestB "CS6620" & "620"
     ldr  param2, =part2_str3                        @   Params String1, String2
     bl   _fnFindSubString                           @
-	mov   param1, temp                              @ Store the Result and increment
-	bl   _fnStoreResult                             @    Result Store Index
-	add  temp, #4                                   @ 
+    mov   param1, temp                              @ Store the Result and increment
+    bl   _fnStoreResult                             @    Result Store Index
+    add  temp, #4                                   @
 
     ldr  param1, =part2_str1                        @ Part 2 TestB "CS6620" & "6U"
     ldr  param2, =part2_str4                        @   Params String1, String2
     bl   _fnFindSubString                           @
-	mov   param1, temp                              @ Store the Result and increment
-	bl   _fnStoreResult                             @    Result Store Index
+    mov   param1, temp                              @ Store the Result and increment
+    bl   _fnStoreResult                             @    Result Store Index
 
     bl   _end_of_program                            @ End of the program is here
 
@@ -124,11 +124,11 @@ _fnFindSubString:                                   @ Find SubString Index
   _fnFindSubString_loop:                            @ Loop to look for first character
     ldrb  temp1, [param1, str1_Idx]                 @ Read String1 Characters
     add   str1_Idx,  #1                             @ Increment String1 Index
-	movs  temp1, temp1                              @ Check for null character
-	beq   _fnFindSubString_mot_found                @ Sub string not found
+    movs  temp1, temp1                              @ Check for null character
+    beq   _fnFindSubString_mot_found                @ Sub string not found
     cmp   temp1, temp2                              @ Compare Characters and continue searching
     bne   _fnFindSubString_loop                     @   if characters are not matching otherwise -
-    mov   sub_str_idx, str1_Idx                     @   continue with substring matching 
+    mov   sub_str_idx, str1_Idx                     @   continue with substring matching
     sub   str1_Idx, #1                              @
   _fnFindSubString_loop2:                           @ Substring Matching loop
     add   str1_Idx,  #1                             @ Increment String1 Index
@@ -141,9 +141,9 @@ _fnFindSubString:                                   @ Find SubString Index
     beq   _fnFindSubString_loop2                    @    if values are matching otherwise -
     movs  temp1, temp1                              @    check of end string1 and restart searching -
     bne   _fnFindSubString_find                     @    Stop if null char found.
-  _fnFindSubString_mot_found:                       @ 
+  _fnFindSubString_mot_found:                       @
     movs  sub_str_idx, #0                           @ Reset Result
-  _fnFindSubString_found:                           @ 
+  _fnFindSubString_found:                           @
     movs  retVal, sub_str_idx                       @ Update Result, This contain substring index
   _fnFindSubString_end:                             @
     pop   {temp1, temp2}                            @ Restore temp variables from stack
@@ -170,10 +170,10 @@ _fnCompareString:                                   @ Compare two strings
 /*** Function: storeResult **********************************************************************/
 _fnStoreResult:                                     @ Store Result, Recevies Index and retVal
     push  {temp, lr}
-	ldr   temp, =result_start
-	str   retVal, [temp1, param1]
+    ldr   temp, =result_start
+    str   retVal, [temp1, param1]
     pop   {temp, pc}
-	
+
 /*** Function: strlen ***************************************************************************/
 _fnstrlen:                                          @ Calculate Length of a String
     push  {temp, lr}
